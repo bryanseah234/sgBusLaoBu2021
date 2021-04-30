6f63f2d0e16ea93e1100a79136e762a247d68106
bryanseah234 <>
2021-04-30 15:01:06 +0000

    Initial commit

---
+++ schema/DATASETS.py
@@ -0,0 +1,26 @@
+'''EXAMPLE OF DATASET'''
+#routes
+    # "ServiceNo": "10",
+    # "Direction": 1,
+    # "StopSequence": 1,
+    # "BusStopCode": "75009",
+    # "WD_FirstBus": "0500",
+    # "WD_LastBus": "2300",
+    # "SAT_FirstBus": "0500",
+    # "SAT_LastBus": "2300",
+    # "SUN_FirstBus": "0500",
+    # "SUN_LastBus": "2300"
+
+# service
+    # "ServiceNo": "118",
+    # "Direction": 1,
+    # "AM_Peak_Freq": "5-08",
+    # "AM_Offpeak_Freq": "8-12",
+    # "PM_Peak_Freq": "8-10",
+    # "PM_Offpeak_Freq": "09-14",
+
+#stop
+    # "BusStopCode": "01012",
+    # "Description": "Hotel Grand Pacific",
+    # "Latitude": 1.29684825487647,
+    # "Longitude": 103.85253591654006
